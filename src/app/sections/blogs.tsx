7adf7a3df5221e8c8096939bd3dda6c5c1219ec4
GhaziDev <ghazi-z3balawi@outlook.com>
2024-12-13 21:09:42 +1300

    removed unused hook from import

--- src/app/sections/blogs.tsx
+++ src/app/sections/blogs.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 
 import {Pagination} from "@nextui-org/react"
 import { PartialBlog } from '../interfaces/interfaces'
